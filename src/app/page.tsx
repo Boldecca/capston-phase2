a8bb56bc0fda9f92d88ed362203fcf31b7309252
Boldecca <muteterirebecca60@gmail.com>
2025-11-14 10:58:26 +0200

    remove gallery section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import Hero from "@/components/Hero";
 import FeaturedStories from "@/components/FeaturedStories";
 import CTABanner from "@/components/CTABanner";
-import Gallery from "@/components/Gallery";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <FeaturedStories />
-      <Gallery />
       <CTABanner />
     </>
   );
